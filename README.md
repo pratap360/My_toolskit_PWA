c9d12cec98f0b85cdfbc4d00b8238ef8ef6c8a66
pratap360 <pratapparui0360@gmail.com>
2022-01-22 19:47:50 +0530

    updates all files and folders

---
+++ README.md
@@ -0,0 +1,9 @@
+# My_toolskit_PWA
+This it the SPM / PI Final year project of bscit 
+
+Created and edited by Rahmat ali and pratap parui
+
+- Front end done by - Pratap Parui 
+- Back end done by - Rahamat ali 
+
+## this is under developement state !
---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(".sidenav").sidenav();
+    $(".slider").slider({ full_width: true });
+    $(".dropdown-trigger").dropdown({ hover: false });
+    $('.modal').modal();
+    $('.collapsible').collapsible();  
+    $('select').formSelect(); 
+    $('.tabs').tabs();
+    // $('.datepicker').datepicker(); 
+});
+
+function toggleModal() {
+    var instance = M.Modal.getInstance($("#modal3"));
+    instance.open();
+}
