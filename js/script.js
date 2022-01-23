c6f285efaa3d340051bb0bf1f1ae567fd72555dc
pratap360 <pratapparui0360@gmail.com>
2022-01-23 21:38:58 +0530

    added about us & changes some files

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
     $('select').formSelect(); 
     $('.tabs').tabs();
     // $('.datepicker').datepicker(); 
+    $('.chips').chips();
 });
 
 function toggleModal() {
